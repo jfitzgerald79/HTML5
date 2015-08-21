e8b570eadd2952550517eb33f924376f7e1d9c25
James Fitzgerald <nimblewillcreek@gmail.com>
2015-08-21 16:01:47 -0400

    Update layout.js

--- javascript/layout.js
+++ javascript/layout.js
@@ -958,34 +958,34 @@ function addLayerList(layers) {
         //dojo.byId('webmap-toolbar-center').appendChild(button.domNode);
         dojo.byId('webmap-toolbar-left').appendChild(button.domNode);
         
-        dojo.connect(button, 'onClick', function () {
-            navigateStack('legendPanel');
-        });
-        var layerCp = new dijit.layout.ContentPane({
-            title: i18n.tools.layer.title,
-            selected: true,
-            region: 'center',
-            id: "legendPanel"
-        });
+  //     dojo.connect(button, 'onClick', function () {
+   //         navigateStack('legendPanel');
+      //  });
+     //   var layerCp = new dijit.layout.ContentPane({
+      //      title: i18n.tools.layer.title,
+      //      selected: true,
+      //      region: 'center',
+      //      id: "legendPanel"
+      // });
         
-        dijit.byId('stackContainer').addChild(layerCp);
-        dojo.addClass(dojo.byId('legendPanel'), 'panel_content');
+      //  dijit.byId('stackContainer').addChild(layerCp);
+       // dojo.addClass(dojo.byId('legendPanel'), 'panel_content');
 
-        var layerDijit = new esri.dijit.Layers({
-            map: map,
-            layerInfos: layerInfo
-        }, dojo.create('div'));
+       // var layerDijit = new esri.dijit.Layers({
+       //     map: map,
+       //     layerInfos: layerInfo
+       // }, dojo.create('div'));
 
-        dojo.byId('legendPanel').appendChild(layerDijit.domNode);
+        //dojo.byId('legendPanel').appendChild(layerDijit.domNode);
 
-        navigateStack('layerPanel');
-        If (dojo.isIE === 8) {
-            setTimeout(function () {
-               layerDijit.startup();
-            }, 100);
-        } else {
-            layerDijit.startup();
-        }
+       // navigateStack('layerPanel');
+        //If (dojo.isIE === 8) {
+        //    setTimeout(function () {
+        //       layerDijit.startup();
+        //    }, 100);
+       // } else {
+        //    layerDijit.startup();
+       // }
     }
 }
 
